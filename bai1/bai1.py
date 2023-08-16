d8bbc97a6c2f3ecd0a402995d5dfcb7377e122bd
ducluan2001 <ndluan1212@gmail.com>
2023-08-16 16:19:27 +0700

    coban1

---
+++ bai1/bai1.py
@@ -0,0 +1,6 @@
+# vd 1.3
+year = 2020
+if (year % 4 == 0 and year % 100 != 0 or year % 400 == 0):
+    print(year, "la nam nhuan")
+else:
+    print(year, "khong la nam nhuan")
---
+++ bai1/bai2.py
@@ -0,0 +1,9 @@
+# vd 1.4
+a = 2
+b = 113
+if (a == 0 and b == 0):
+    print("phuong trinh co vo so nghiem")
+if (a == 0 and b != 0):
+    print("phuong trinh vo nghiem ")
+else:
+    print("phuong trinh co nghiem x =", -b/a)
