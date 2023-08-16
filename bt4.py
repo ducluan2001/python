fc9c2a31b7b01f631c421d46780cf7cc915032be
ducluan2001 <ndluan1212@gmail.com>
2023-08-16 16:46:57 +0700

    btvn

---
+++ bt4.py
@@ -0,0 +1,4 @@
+print("Moi ban chon chuc nang: ")
+print("1. Dang nhap he thong")
+print("2. Dang xuat he thong")
+print("3. Thoat khoi he thong")    
---
+++ bt5.py
@@ -0,0 +1,7 @@
+print("HO TEN: NONG DUC LUAN")
+print("TUOI: 22")
+print("GIOI TINH: NAM")
+print("SO DIEN THOAI: 096998987")
+print("EMAIL: NDLUAN1212@GMAIL.COM")
+print("DIA CHI: BAC KAN")
+print("NGHE NGHIEP: IT")
