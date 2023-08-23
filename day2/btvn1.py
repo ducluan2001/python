8a8b596f912cea0ea5ad10b83ac9ae8b7be9cf6a
ducluan2001 <ndluan1212@gmail.com>
2023-08-23 16:58:40 +0700

    buoi3

--- day2/btvn1.py
+++ day2/btvn1.py
@@ -1,6 +1,10 @@
-# in ra he bat phan
-a = 8
+# chuyển hệ thập phân sang hệ bát phân
+a = 6
 print("%o" % (a))
-# in ra 2 so sau dau phay
+# in ra 2 số sau dấu phẩy
 b = 9.3123413
 print("%.2f" % (b))
+# tính 2 mũ n không dùng hàm
+
+n = 1 << 10
+print(n)
