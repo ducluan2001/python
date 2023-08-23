b51509ad87dac406acadced5189cab7796f29ba3
ducluan2001 <ndluan1212@gmail.com>
2023-08-23 15:52:15 +0700

    buoi3

--- day2/bai1.py
+++ day2/bai1.py
@@ -11,6 +11,8 @@ kq = math.sqrt(a)  # can bac 2 cua a
 
 print("Can bac 2 cua", a, "la:", math.sqrt(a))
 
+
+'''làm việc với chuỗi'''
 # toán tử in kiểm tra xem chuỗi con có trong chuỗi mẹ hay không
 
 ok = "hi ma lai a"
@@ -18,9 +20,14 @@ ok1 = "hi" in ok
 print(ok1)
 
 # xu lý chuỗi
-
 tnc = "cao dang thai nguyen"
+# in cach chuoi tnc[vi tri bat dau: vi tri ket thuc: chi so buoc nhay]
 print(tnc[0:8])  # in ra chuoi con
-# in cach chuoi tnc[vi tri bat dau: vi tri ket thuc: chi so bo qua]
 print(tnc[0:8:2])
 print(tnc[::-1])  # dao nguoc chuoi
+
+'''de xuong dong dung dau \n
+-tab dung dau \t
+-doi voi chuoi co the dung mot so toan tu + hoac * voi chuoi
+
+'''
