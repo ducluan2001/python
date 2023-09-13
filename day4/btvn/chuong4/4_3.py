776febc529c7ac66efc72c12028e030f915704ac
ducluan2001 <ndluan1212@gmail.com>
2023-09-13 14:55:56 +0700

    ok

--- day4/btvn/chuong4/4_3.py
+++ day4/btvn/chuong4/4_3.py
@@ -1,4 +1,3 @@
-
 import math
 
 h = int(input("Nhap so gio: "))
@@ -8,10 +7,19 @@ k = int(input("Nhap so giay cong them: "))
 
 s += k
 
+# cách 1
 while (s >= 60):
     m += 1
     s -= 60
     if (m >= 60):
         h += 1
         m -= 60
+
+print(h, m, s)
+
+# cách 2
+sum_s = h * 3600 + m * 60 + s + k
+h = int(sum_s / 3600)
+m = int((sum_s % 3600) / 60)
+s = sum_s - h * 3600 - m * 60
 print(h, m, s)
--- day4/btvn/chuong4/4_4_1.py
+++ day4/btvn/chuong4/4_4_1.py
@@ -4,12 +4,30 @@ import math
 a = int(input("Nhap so a: "))
 b = int(input("Nhap so b: "))
 
-while(a != b):
-    if(a > b):
-        a = a - b
-        # print("a",a)
-    else:
-        b = b - a
-        # print(b)
+# cách 1
+# while(a != b):
+#     if(a > b):
+#         a = a - b
+#         # print("a",a)
+#     else:
+#         b = b - a
+#         # print(b)
+# print(a)
 
-print(a)
+# cách 2
+# while(a * b):
+#     if(a > b):
+#         a = a % b
+#     else:
+#         b = b % a
+# print(a + b)
+
+
+def x(a, b):
+    if (b == 0):
+        return a
+    else:
+        return x(b, a % b)
+
+
+print(x(a, b))
