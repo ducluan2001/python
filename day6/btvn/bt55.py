f27da14cf9c68615002f2535a9d28f6de3ec4831
ducluan2001 <127734124+ducluan2001@users.noreply.github.com>
2023-10-01 04:02:58 +0000

    btvn

---
+++ day6/btvn/bt55.py
@@ -0,0 +1,23 @@
+n = int(input("Nhap so luong bang dia: "))
+
+ds = []
+tt = {"ten": '', "the loai": '', "so luong": '',"gia": ''}
+
+for i in range(n):
+    print("Nhap thong tin sinh bang dia", i+1, ":")
+    for j in tt:
+        print("Nhap", j, end=":")
+        tt[j] = input()
+    ds.append(tt.copy())
+# print(ds)
+ds2 = []
+min = ds[0]["gia"]
+for i in range(n):
+    print("Ten", ds[i]["ten"], "|The loai", ds[i]["the loai"], "|So luong", ds[i]["so luong"], "|Gia", ds[i]["gia"])
+    if(ds[i]["gia"] < min):
+        min = ds[i]["gia"]
+        ds2.append(ds[i])
+    else:
+        ds2.append(ds[0])
+print(ds2)
+
