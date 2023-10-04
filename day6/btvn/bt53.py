fdc68aa9af1705c9242a5e66a05e3fff6fa7d78a
ducluan2001 <ndluan1212@gmail.com>
2023-10-04 13:22:40 +0700

    btnv

--- day6/btvn/bt53.py
+++ day6/btvn/bt53.py
@@ -12,9 +12,15 @@ for i in range(n):
             tt[j] = input()
     ds.append(tt.copy())
 sum = 0
+ds2 = []
+ds3 = []
 print("Danh sach hang hoa co so luong tren 50 sp: ")
 for i in range(n):
     sum += ds[i]["so luong"]
+    if (ds[i]["so luong"] < 10):
+        ds2.append(ds[i])
     if (ds[i]["so luong"] > 50):
-        print("-------", ds[i])
+        ds3.append(ds[i])
 print("Tong so luong", sum)
+print("danh sach mat hang duoi 10", ds2)
+print("danh sach mat hang tren 50", ds3)
--- day6/btvn/bt55.py
+++ day6/btvn/bt55.py
@@ -1,7 +1,7 @@
 n = int(input("Nhap so luong bang dia: "))
 
 ds = []
-tt = {"ten": '', "the loai": '', "so luong": '',"gia": ''}
+tt = {"ten": '', "the loai": '', "so luong": '', "gia": ''}
 
 for i in range(n):
     print("Nhap thong tin sinh bang dia", i+1, ":")
@@ -13,15 +13,12 @@ for i in range(n):
 ds2 = []
 min = ds[0]["gia"]
 for i in range(n):
-    print("Ten", ds[i]["ten"], "|The loai", ds[i]["the loai"], "|So luong", ds[i]["so luong"], "|Gia", ds[i]["gia"])
-    if(ds[i]["gia"] < min):
+    print("Ten", ds[i]["ten"], "|The loai", ds[i]["the loai"],
+          "|So luong", ds[i]["so luong"], "|Gia", ds[i]["gia"])
+    if (ds[i]["gia"] < min):
         min = ds[i]["gia"]
-<<<<<<< HEAD
         ds2 = []
-=======
->>>>>>> 7a43ad948569d0206a6f839fba43f2c19fd299f3
         ds2.append(ds[i])
-if(ds2 == []):
+if (ds2 == []):
     ds2.append(ds[0])
 print(ds2)
-
