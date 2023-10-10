74cf8b1bc8027d3a439628b14317b7029887aaef
ducluan2001 <ndluan1212@gmail.com>
2023-10-10 17:21:49 +0700

    bt8

---
+++ day8/apandas.py
@@ -0,0 +1,17 @@
+import pandas
+# nhap file
+file = open('datapandas.txt', 'a')
+while (True):
+    maSv = input("Nhap ma sinh vien: ")
+    if (maSv == ""):
+        break
+    tenSv = input("Nhap ten sinh vien: ")
+    lop = input("Nhap lop sinh vien: ")
+    quequan = input("Nhap que quan sinh vien: ")
+    file.write(','.join([maSv, tenSv, lop, quequan]) + '\n')
+file.close()
+
+# doc file
+file2 = pandas.read_csv('datapandas.txt', sep=',', header=None, names=[
+                        'masv', 'tensv', 'lop', 'quequan'])
+print(file2)
